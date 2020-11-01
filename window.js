3c4d90d00335bf0d65a2b56c4bcdd4f91f9b852d
Ilias Apostolou <ilias.apostolou.zero@gmail.com>
2020-11-01 14:20:20 +0200

    Script: npm run start

--- window.js
+++ window.js
@@ -9,7 +9,7 @@ let mainWindow;
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 1200,
+    width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true
