85c8a04b1d1f225edbfac16c8664c01a2e5b9831
Laufer4u <nzevgolis4u@gmail.com>
2020-11-08 20:22:41 +0200

    webpack working

--- web/script.js
+++ web/script.js
@@ -1,12 +1,1 @@
-
-
-console.log('main');
-
-var d = new Date("2015-03-25");
-
-
-var btn = document.createElement("BUTTON");
-btn.innerHTML = "Add Phone"
-document.body.appendChild(btn);
-
-//var input = document.createElement("input");
+(()=>{console.log("main"),new Date("2015-03-25");var e=document.createElement("BUTTON");e.innerHTML="Add Phone",document.body.appendChild(e)})();
