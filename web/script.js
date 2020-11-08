bac2a9d40a66fa6e8364a699dd84bd10651efd08
Laufer4u <nzevgolis4u@gmail.com>
2020-11-08 19:51:41 +0200

    -

--- web/script.js
+++ web/script.js
@@ -1,3 +1,12 @@
 
 
 console.log('main');
+
+var d = new Date("2015-03-25");
+
+
+var btn = document.createElement("BUTTON");
+btn.innerHTML = "Add Phone"
+document.body.appendChild(btn);
+
+//var input = document.createElement("input");
